e0f37a9880c60e3f53eeb9eaa31ad345891ee625
santiago459-dev <devgnar459@gmail.com>
2025-02-01 22:05:29 -0500

    fixed bugs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,10 @@ import { defineConfig } from "astro/config";
 import icon from "astro-icon";
 import tailwindcss from "@tailwindcss/vite";
 
-import preact from "@astrojs/preact";
-
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [icon(), preact()],
+  integrations: [icon()],
 });
